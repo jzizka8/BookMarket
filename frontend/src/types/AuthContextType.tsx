722cb0f62d294f4bb16ec61420811de449bf7b76
xsieklov <xsieklov@fi.muni.cz>
2023-06-01 16:07:07 +0200

    fix: remove types

--- frontend/src/types/AuthContextType.tsx
+++
@@ -1,9 +0,0 @@
-import UserDataType from './UserDataType';
-
-type AuthContextType = {
-  user: UserDataType | null;
-  login: (data: UserDataType) => Promise<void>;
-  logout: () => void;
-};
-
-export default AuthContextType;
