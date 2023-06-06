a47ef2b106a0a320ee3cbff1ec51f4db3f3b3364
Diana Janickova <janickova.diana@gmail.com>
2023-06-06 15:32:58 +0200

    fix: check uniqueness of category name

--- backend/src/repositories/category/create.ts
+++ backend/src/repositories/category/create.ts
@@ -2,17 +2,33 @@ import { Result } from '@badrap/result';
 import type { Category } from '@prisma/client';
 import client from '../client';
 import type { CategoryCreateData } from './types';
+import { ConflictingRecordError } from '../types/errors';
 
 /**
  * Repository call that creates a category.
  *
  * @param data object containing necessary data to create a category
  * @returns - On success: the created category
- *          - On failure: a generic error
+ *          - On failure: ConflictingRecordError if category with given name already exists
+ *                        a generic error
  */
 
 const create = async (data: CategoryCreateData): Promise<Result<Category>> => {
   try {
+    const existingCategory = await client.category.findFirst({
+      where: {
+        name: data.name,
+      },
+    });
+
+    if (existingCategory) {
+      return Result.err(
+        new ConflictingRecordError(
+          'Category with the given name already exists'
+        )
+      );
+    }
+
     const category = await client.category.create({
       data: { ...data },
     });
