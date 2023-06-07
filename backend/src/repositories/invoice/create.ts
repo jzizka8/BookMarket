0ca32bd23fcba5d7bdd25eca45ef77227ea9f59b
Jakub Ulicny <xulicny@fi.muni.cz>
2023-06-07 11:17:45 +0200

    feat: invoice create

---
+++ backend/src/repositories/invoice/create.ts
@@ -0,0 +1,63 @@
+import { Result } from "@badrap/result";
+import prisma from "../client";
+import { specific } from "../user/read";
+import type { InvoiceCreateData, InvoiceCreateResult } from "./types";
+import { UserNotFound } from "./types/errors";
+
+/**
+ * Repository call that creates a User.
+ *
+ * @param data - username 
+ *             - hashedPassword
+ * @returns - Result.ok({ User }) on success
+ *          - NotUniqueUsernameError if the username is not unique
+ *          - Result.err(_) otherwise
+ */
+const create = async (data: InvoiceCreateData): InvoiceCreateResult => {
+  try {
+
+    const { userData, address, ...invoiceData } = data;
+
+    const user = await specific({ id: data.userId });
+    if (user.isErr) {
+      return Result.err(new UserNotFound('User with this id does not exist!'));
+    }
+    
+    const books = await prisma.book.findMany({
+      where: {
+        id: {
+          in: data.bookId
+        }
+      }
+    })
+
+    const invoice = await prisma.invoice.create({
+      data: {
+        buyerId: invoiceData.userId,
+        amount: invoiceData.amount,
+        name: userData.name,
+        surname: userData.surname,
+        email: userData.email,
+        phoneNumber: userData.phoneNumber,
+        street: address.street,
+        city: address.city,
+        zipcode: address.zipcode,
+        country: address.country,
+        books: {
+          connect: books.map((book) => ({ id: book.id })),
+        }
+      },
+      include: {
+        buyer: true
+      },
+    }
+    );
+
+    return Result.ok(invoice);
+
+  } catch (e) {
+    return Result.err(e as Error);
+  }
+}
+
+export default create;
---
+++ backend/src/repositories/invoice/types.ts
@@ -0,0 +1,32 @@
+import type { Result } from "@badrap/result"
+import type { Invoice, User } from "@prisma/client"
+
+type UserData = {
+  name: string,
+  surname: string,
+  email: string,
+  phoneNumber: string
+}
+
+type Address = {
+  street: string,
+  city: string,
+  zipcode: string,
+  country: string
+}
+
+export type InvoiceCreateData = {
+  userId: string,
+  bookId: string[],
+  amount: number,
+  userData: UserData,
+  address: Address
+}
+
+export type InvoiceCreateResult = Promise<Result<Invoice & {buyer: User}>>;
+
+export type InvoiceReadSpecificData = {
+  buyerId: string
+}
+
+export type InvoiceReadSpecificResult = Promise<Result<Invoice[] & User>>;
