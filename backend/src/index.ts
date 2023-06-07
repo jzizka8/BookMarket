4ead31fa54ab2e262dd6fd78596ac3e2ec3727fe
Diana Janickova <janickova.diana@gmail.com>
2023-06-07 18:11:51 +0200

    fix: remove console statement

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,2 @@
-import { all } from './repositories/book/read';
-
 // eslint-disable-next-line no-console
 console.log('Hello world');
-
-async function run() {
-  const actual = await all();
-
-  console.log(actual);
-}
-
-run();
