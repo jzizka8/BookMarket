1c2fde2c187cbc034800bd039873874e6971be23
Jakub Ulicny <xulicny@fi.muni.cz>
2023-06-11 12:41:25 +0200

    feat: squa sh

---
+++ backend/src/controllers/user/read.ts
@@ -0,0 +1,49 @@
+import type { Request, Response } from 'express';
+import { signInSchema, specificSchema } from '../../schemas/userSchemas';
+import { specific, login } from '../../repositories/user/read';
+import { failResponse, loadFailedResponse } from '../common';
+
+const specificUser = async (req: Request, res: Response) => {
+  try {
+    // Validation
+    const queryValidate = await specificSchema.parse(req.params);
+    
+    // Repo call
+    const user = await specific(queryValidate);
+    
+    // Checking repo answer and returning
+    if (user.isErr) {
+      return loadFailedResponse(res);
+    }
+    return res.status(201).send({
+      data: user.unwrap(),
+    });
+  } catch (e) {
+    return failResponse(res, e);
+  }
+}
+
+const userLogin = async (req: Request, res: Response) => {
+  try {
+    // Validation
+    const queryValidate = signInSchema.parse(req.body);
+
+    // Repo call
+    const user = await login(queryValidate);
+
+    // Checking repo answer and returning
+    if (user.isErr) {
+      return loadFailedResponse(res);
+    }
+    return res.status(201).send({
+      data: user.unwrap(),
+    });
+  } catch (e) {
+    return failResponse(res, e);
+  }
+};
+
+export default {
+  userLogin,
+  specificUser
+};
