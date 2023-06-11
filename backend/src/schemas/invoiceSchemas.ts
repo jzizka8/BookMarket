ae10988cee91fa9618b70070a1b9ffee90e2504d
Jakub Ulicny <xulicny@fi.muni.cz>
2023-06-11 16:19:28 +0200

    fix: invoice createSchema type

--- backend/src/schemas/invoiceSchemas.ts
+++ backend/src/schemas/invoiceSchemas.ts
@@ -3,18 +3,18 @@ import { z } from "zod"
 const userData = z.object({
   name: z.string()
   .min(2, "We are sorry if you have name which is less than 2 characters long, but we don't support that."),
-  city: z.string().nonempty(),
-  zipcode: z.string().length(5, "ZIP Codes are usually 5 characters long."),
-  contry: z.string().min(4, "Shortest country name has 4 characters.")
+  surname: z.string().nonempty(),
+  email: z.string().email("This is not valid email."),
+  phoneNumber: z.string()
+  .regex(new RegExp('/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/'),
+  "This is not valid phone number.")
 })
 
 const address = z.object({
   street: z.string().nonempty(),
   city: z.string().nonempty(),
-  email: z.string().email("This is not valid email."),
-  phoneNumber: z.string()
-  .regex(new RegExp('/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/'),
-  "This is not valid phone number.")
+  zipcode: z.string().length(5, "ZIP Codes are usually 5 characters long."),
+  country: z.string().min(4, "Shortest country name has 4 characters.")
 })
 
 export const createBodySchema = z.object({
