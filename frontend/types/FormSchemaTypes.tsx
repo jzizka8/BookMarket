fa0395e0bef6833972d7ea634cc6f9f4c87a1624
xsieklov <xsieklov@fi.muni.cz>
2023-06-13 13:07:42 +0200

    feat: payment info form

--- frontend/types/FormSchemaTypes.tsx
+++ frontend/types/FormSchemaTypes.tsx
@@ -2,7 +2,9 @@ import { z } from 'zod';
 import registerFormSchema from '../src/schemas/RegisterFormSchema';
 import loginFormSchema from '../src/schemas/LoginFormSchema';
 import newBookSchema from '../src/schemas/NewBookSchema';
+import paymentInfoSchema from '../src/schemas/PaymentInfoSchema';
 
 export type RegisterFormSchemaType = z.infer<typeof registerFormSchema>;
 export type LoginFormSchemaType = z.infer<typeof loginFormSchema>;
 export type NewBookSchemaType = z.infer<typeof newBookSchema>;
+export type PaymentInfoSchemaType = z.infer<typeof paymentInfoSchema>;
