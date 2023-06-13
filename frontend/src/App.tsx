fc5bfd2f22aca09ceae4a1b61161d054bdd85036
xsieklov <xsieklov@fi.muni.cz>
2023-06-13 10:17:36 +0200

    fix: fix routing to homepage and to missing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Missing from './pages/Missing';
@@ -12,16 +12,26 @@ import PaymentInfo from './pages/PaymentInfo';
 import OrderConfirmation from './pages/OrderConfirmation';
 import Navbar from './components/Navbar';
 import UserOrders from './pages/UserOrders';
+import { useEffect } from 'react';
 
 export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const { pathname } = window.location;
+    if (pathname === '/') {
+      navigate('/books');
+    }
+  }, [navigate]);
+
   return (
     <>
       <header>
         <Navbar></Navbar>
       </header>
       <Routes>
-        <Route path="/" element={<AllBooks />} />
-        <Route path="/:bookId" element={<BookDetail />} />
+        <Route path="/books" element={<AllBooks />} />
+        <Route path="/books/:bookId" element={<BookDetail />} />
         <Route path="/userBooks/:userId" element={<UserBooksForSale />} />
         <Route path="/userOrders/:userId" element={<UserOrders />} />
         <Route path="/bookAddition" element={<BookAddition />} />
@@ -32,7 +42,7 @@ export const App = () => {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Missing />} />
+        <Route path="/*" element={<Missing />} />
       </Routes>
     </>
   );
