d7493a7e4aee4b64e5b3fb6193da794924551e94
Josef Zizka <josefzizka21@gmail.com>
2023-06-17 17:28:14 +0200

    feat: resetup tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/compiled.css';
+import './styles/index.css';
 import App from './App';
-import './styles/compiled.css';
 import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
