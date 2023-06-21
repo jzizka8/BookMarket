1bf675fcf74b5038c8de74e1f53af79ce22f04a0
Jakub Ulicny <xulicny@fi.muni.cz>
2023-06-21 20:47:56 +0200

    fix: lint

--- frontend/src/pages/BookAddition.tsx
+++ frontend/src/pages/BookAddition.tsx
@@ -2,7 +2,7 @@ import NewBookForm from '../components/NewBookForm';
 const BookAddition = () => {
   return (
     <>
-      <div className="flex min-h-fit flex-col items-center justify-center text-center m-auto">
+      <div className="m-auto flex min-h-fit flex-col items-center justify-center text-center">
         <div>
           <h1 className="mb-4 py-2.5 text-3xl font-bold leading-tight text-gray-900">
             Let a book live its second life
