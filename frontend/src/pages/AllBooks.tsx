ce62fa6b26d2538b0dafad75b6d1625aec50420c
Josef Zizka <josefzizka21@gmail.com>
2023-06-21 23:49:39 +0200

    fix: layout

--- frontend/src/pages/AllBooks.tsx
+++ frontend/src/pages/AllBooks.tsx
@@ -117,16 +117,18 @@ const AllBooks = () => {
       ) : (
         <>
           <div className="flex justify-center">
-            <div className="flex h-fit justify-center bg-slate-100">
-              {books.map((book: Book) => (
-                <BookCard
-                  key={book.id}
-                  {...{
-                    book,
-                  }}
-                  showRemoveButton={false}
-                />
-              ))}
+            <div className="flex h-fit justify-center">
+              <div className="mt-5 grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                {books.map((book: Book) => (
+                  <BookCard
+                    key={book.id}
+                    {...{
+                      book,
+                    }}
+                    showRemoveButton={false}
+                  />
+                ))}
+              </div>
             </div>
           </div>
           <div className="my-4 flex justify-center">
